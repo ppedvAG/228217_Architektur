baadad54f96ded3babacd510cf3267b7d493c964
André <AndreR@ppedv.de>
2023-07-03 11:20:54 +0200

    Overflowtests

--- Calculator/Calculator.Tests/CalcTests.cs
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -14,5 +14,44 @@ namespace Calculator.Tests
             //Assert
             Assert.Equal(7, result);
         }
+
+        [Fact]
+        public void Sum_0_and_0_results_0()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(4, 6, 10)]
+        [InlineData(-4, -3, -7)]
+        [InlineData(-4, 7, 3)]
+        public void Sum_tests(int a, int b, int exp)
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(a, b);
+
+            //Assert
+            Assert.Equal(exp, result);
+        }
+
+        [Fact]
+        public void Sum_MAX_and_1_throws_OverflowException()
+        {
+            Calc calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
     }
 }
