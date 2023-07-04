317288e3f63392a8fc3253f28971ba3d8c69af89
André <AndreR@ppedv.de>
2023-07-04 11:39:07 +0200

    Google Books Client (schlimm)

---
+++ BooksAPIClient/BooksAPIClient/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BooksAPIClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GoSuche(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={tb1.Text}";
+
+            var http = new HttpClient();
+            var json = await http.GetStringAsync(url);
+
+            var result = System.Text.Json.JsonSerializer.Deserialize<BooksResult>(json);
+
+            myGrid.ItemsSource = result.items.Select(x => x.volumeInfo).ToList();
+
+        }
+
+        private async void CalcPageSum(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={tb1.Text}";
+
+            var http = new HttpClient();
+            var json = await http.GetStringAsync(url);
+
+            var result = System.Text.Json.JsonSerializer.Deserialize<BooksResult>(json);
+
+
+            MessageBox.Show($"{result.items.Sum(x => x.volumeInfo.pageCount)} pages");
+        }
+    }
+}
