f5f00e628134a6bd12e3d8b43d45cd5951bd777c
André <AndreR@ppedv.de>
2023-07-04 16:39:00 +0200

    Factory 🏭

---
+++ HalloFactory/HalloFactory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Common;
+
+Console.WriteLine("Hello Factory");
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=Northwnd;Trusted_Connection=true"; ;
+
+//DbProviderFactory factory = Microsoft.Data.SqlClient.SqlClientFactory.Instance;
+DbProviderFactory factory = Npgsql.NpgsqlFactory.Instance;
+
+DbConnection connection = factory.CreateConnection();
+connection.ConnectionString = conString;
+connection.Open();
+
+DbCommand command = connection.CreateCommand();
+command.CommandText = "SELECT * FROM Employees";
+DbDataReader reader = command.ExecuteReader();
+while(reader.Read())
+{
+    Console.WriteLine(reader.GetString(reader.GetOrdinal("FirstName")));
+    Console.WriteLine(reader.GetDateTime(reader.GetOrdinal("BirthDate")).ToShortDateString());
+}
+
+
+
+
+
+
