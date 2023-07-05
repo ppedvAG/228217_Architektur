579bfd90380960f1ab23e25f242d5b4f8b313738
André <AndreR@ppedv.de>
2023-07-05 09:37:24 +0200

    Deko Pattern

---
+++ HalloDeko/HalloDeko/Component.cs
@@ -0,0 +1,54 @@
+﻿namespace HalloDeko
+{
+    internal interface IComponent
+    {
+        string Name { get; }
+        decimal Price { get; }
+    }
+
+    class Pizza : IComponent
+    {
+        public string Name => "Pizza";
+
+        public decimal Price => 7m;
+    }
+
+    class Brot : IComponent
+    {
+        public string Name => "Brot";
+
+        public decimal Price => 3m;
+    }
+
+    abstract class Deco : IComponent
+    {
+        protected readonly IComponent _parent;
+
+        protected Deco(IComponent parent)
+        {
+            _parent = parent;
+        }
+
+        public abstract string Name { get; }
+        public abstract decimal Price { get; }
+    }
+
+    class Käse : Deco
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => _parent.Name + " Käse";
+
+        public override decimal Price => _parent.Price + 3m;
+    }
+    class Salami : Deco
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => _parent.Name + " Salami";
+
+        public override decimal Price => _parent.Price + 4m;
+    }
+}
---
+++ HalloDeko/HalloDeko/Program.cs
@@ -0,0 +1,10 @@
+﻿using HalloDeko;
+
+Console.WriteLine("Hello, World!");
+
+var p1 = new Käse( new Brot());
+
+Console.WriteLine($"{p1.Name} {p1.Price:c}");
+
+var p2 = new Salami(new Käse(new Käse( new Pizza())));
+Console.WriteLine($"{p2.Name} {p2.Price:c}");
