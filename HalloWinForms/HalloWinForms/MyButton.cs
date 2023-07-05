33eba1a19580f6055f9fb544335fd7eb0660ec03
André <AndreR@ppedv.de>
2023-07-05 12:19:21 +0200

    CustomControls in WinForms == TemplaytePattern

---
+++ HalloWinForms/HalloWinForms/MyButton.cs
@@ -0,0 +1,20 @@
+﻿namespace HalloWinForms
+{
+    public class MyButton : Button
+    {
+
+        int count = 0;
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+
+            pevent.Graphics.FillRectangle(new SolidBrush(Parent.BackColor), ClientRectangle);
+            pevent.Graphics.FillEllipse(Brushes.Cyan, ClientRectangle);
+            pevent.Graphics.DrawEllipse(new Pen(Brushes.DarkCyan, 4f), ClientRectangle);
+            var sf = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+            pevent.Graphics.DrawString($"Paints: {count++}", new Font("Arial", 16f, FontStyle.Bold), Brushes.Black, ClientRectangle, sf);
+
+            count++;
+            //Text = $"Paints: {count++}";
+        }
+    }
+}
