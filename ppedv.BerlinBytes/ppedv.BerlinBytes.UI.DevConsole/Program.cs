421584826835b12cb3318fbbb5937c0a01269395
André <AndreR@ppedv.de>
2023-07-05 16:54:37 +0200

    Dependency Injection fun

--- ppedv.BerlinBytes/ppedv.BerlinBytes.UI.DevConsole/Program.cs
+++ ppedv.BerlinBytes/ppedv.BerlinBytes.UI.DevConsole/Program.cs
@@ -1,12 +1,30 @@
-﻿using ppedv.BerlinBytes.Logic.Core;
+﻿using Autofac;
+using Autofac.Builder;
+using ppedv.BerlinBytes.Data.Db;
+using ppedv.BerlinBytes.Logic.Core;
 using ppedv.BerlinBytes.Model.Contracts;
 using ppedv.BerlinBytes.Model.DomainModel;
+using System.Reflection;
 
 Console.WriteLine("*** BerlinBytes v0.1 PREMIUM EDITION ***");
 
 string conString = "Server=(localdb)\\mssqllocaldb;Database=BerlinBytes_Test;Trusted_Connection=true;";
 
-IRepository repo = new ppedv.BerlinBytes.Data.Db.EfRepository(conString);
+//Di per Reflection
+//var filePath = @"C:\Users\ar2\source\repos\ppedvAG\228217_Architektur\ppedv.BerlinBytes\ppedv.BerlinBytes.Data.Db\bin\Debug\net7.0\ppedv.BerlinBytes.Data.Db.dll";
+//var ass = Assembly.LoadFrom(filePath);
+//var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+//IRepository repo = Activator.CreateInstance(typeMitRepo,conString) as IRepository;
+
+//Di per AutoFac
+var containerBuilder = new ContainerBuilder();
+containerBuilder.Register<EfRepository>(x => new EfRepository(conString)).AsImplementedInterfaces();
+var container = containerBuilder.Build();
+
+IRepository repo = container.Resolve<IRepository>();
+
+//'Di' per code und reference
+//IRepository repo = new ppedv.BerlinBytes.Data.Db.EfRepository(conString);
 ComputerService cs = new ComputerService(repo);
 
 foreach (var computer in repo.GetAll<Computer>())
