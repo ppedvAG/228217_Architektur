4ea5036cc9ce362ec4bd3b5b56ba065762dd31e4
André <AndreR@ppedv.de>
2023-07-06 14:01:16 +0200

    UI mit MVVM

--- ppedv.BerlinBytes/ppedv.BerlinBytes.UI.Desktop/ViewModels/AppsViewModel.cs
+++ ppedv.BerlinBytes/ppedv.BerlinBytes.UI.Desktop/ViewModels/AppsViewModel.cs
@@ -1,15 +1,39 @@
 ﻿using ppedv.BerlinBytes.Model.Contracts;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace ppedv.BerlinBytes.UI.Desktop.ViewModels
 {
-    public class AppsViewModel
+    public class AppsViewModel : INotifyPropertyChanged
     {
+        private Model.DomainModel.App selectedApp;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public List<Model.DomainModel.App> AppList { get; set; }
 
-        public Model.DomainModel.App SelectedApp { get; set; }
+        public Model.DomainModel.App SelectedApp
+        {
+            get => selectedApp;
+            set
+            {
+                selectedApp = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedApp)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VersionCount)));
+            }
+        }
+
+        public string VersionCount
+        {
+            get
+            {
+                if (SelectedApp == null)
+                    return "---";
+
+                return SelectedApp.Versions.Count.ToString();
+            }
+        }
 
         //todo kill it!!
         public AppsViewModel() : this(new Data.Db.EfRepository("Server=(localdb)\\mssqllocaldb;Database=BerlinBytes_Test;Trusted_Connection=true;"))
@@ -20,5 +44,6 @@ namespace ppedv.BerlinBytes.UI.Desktop.ViewModels
         {
             AppList = new List<Model.DomainModel.App>(repo.GetAll<Model.DomainModel.App>().ToList());
         }
+
     }
 }
