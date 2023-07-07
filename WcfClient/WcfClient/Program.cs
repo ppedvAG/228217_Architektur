cbca01bd77142601565a647da0806e0b64ba264b
André <AndreR@ppedv.de>
2023-07-07 11:21:50 +0200

    Wcf Client

---
+++ WcfClient/WcfClient/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+ServiceReference1.BLZServicePortTypeClient client = new ServiceReference1.BLZServicePortTypeClient("");
+
+var result = client.getBankAsync("40050180").Result;
+Console.WriteLine($"{result.bezeichnung}");
