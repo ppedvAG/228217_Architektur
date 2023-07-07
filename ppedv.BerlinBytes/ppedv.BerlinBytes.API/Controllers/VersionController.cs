ad5f50a12a7e28fc50a6e9581e57de51e90eecb0
André <AndreR@ppedv.de>
2023-07-07 11:21:36 +0200

    ASP Web API

---
+++ ppedv.BerlinBytes/ppedv.BerlinBytes.API/Controllers/VersionController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ppedv.BerlinBytes.API.Model;
+using ppedv.BerlinBytes.Model.Contracts;
+using ppedv.BerlinBytes.Model.DomainModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ppedv.BerlinBytes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        private readonly IRepository repo;
+
+        public VersionController(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: api/<VersionController>
+        [HttpGet]
+        public IEnumerable<VersionDTO> Get()
+        {
+            foreach (var item in repo.GetAll<ppedv.BerlinBytes.Model.DomainModel.Version>())
+            {
+                yield return VersionMapper.MapToDTO(item);
+            }
+        }
+
+        // GET api/<VersionController>/5
+        [HttpGet("{id}")]
+        public VersionDTO Get(int id)
+        {
+            return VersionMapper.MapToDTO(repo.GetById<ppedv.BerlinBytes.Model.DomainModel.Version>(id));
+        }
+
+        // POST api/<VersionController>
+        [HttpPost]
+        public void Post([FromBody] VersionDTO value)
+        {
+            repo.Add(VersionMapper.MapToEntity(value));
+            repo.SaveAll();
+        }
+
+        // PUT api/<VersionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] VersionDTO value)
+        {
+        }
+
+        // DELETE api/<VersionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class VersionMapper
+    {
+        public static VersionDTO MapToDTO(ppedv.BerlinBytes.Model.DomainModel.Version version)
+        {
+            return new VersionDTO
+            {
+                Id = version.Id,
+                VersionsNummer = version.Number,
+                Stage = version.Stage.ToString(),
+                ReleaseDate = version.ReleaseDate,
+                EndOfSupportDate = version.EndOfSupportDate,
+                Count = version.DownloadCount
+            };
+        }
+
+        public static ppedv.BerlinBytes.Model.DomainModel.Version MapToEntity(VersionDTO dto)
+        {
+            return new ppedv.BerlinBytes.Model.DomainModel.Version
+            {
+                Id = dto.Id,
+                Number = dto.VersionsNummer,
+                Stage = Enum.Parse<AppStage>(dto.Stage),
+                ReleaseDate = dto.ReleaseDate,
+                EndOfSupportDate = dto.EndOfSupportDate,
+                DownloadCount = dto.Count
+            };
+        }
+    }
+}
---
+++ ppedv.BerlinBytes/ppedv.BerlinBytes.API/Model/VersionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ppedv.BerlinBytes.API.Model
+{
+    public class VersionDTO
+    {
+        public int Id { get; set; }
+        public int VersionsNummer { get; set; }
+        public string Stage { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime EndOfSupportDate { get; set; }
+        public int Count { get; set; }
+    }
+}
